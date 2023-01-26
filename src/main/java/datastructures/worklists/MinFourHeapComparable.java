cbc5c946d891b81ea5d7bd63d251ec44fdee1c2f
Ayush <speedstorm123@gmail.com>
2023-01-26 12:16:37 -0800

    Did Checkpoint 1 (minfourheapcomparable), all tests passed

--- src/main/java/datastructures/worklists/MinFourHeapComparable.java
+++ src/main/java/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,8 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -10,38 +12,100 @@ import cse332.interfaces.worklists.PriorityWorkList;
 public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int size;
+    private int capacity;
 
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+        capacity = 10;
+        data = (E[])new Comparable[capacity];
     }
 
-    @Override
-    public boolean hasWork() {
-        throw new NotYetImplementedException();
-    }
+//    @Override
+//    public boolean hasWork() {
+//        throw new NotYetImplementedException();
+//    }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (size == capacity) {
+            E[] newData = (E[]) new Comparable[capacity * 2];
+            for (int i = 0; i < capacity; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+            capacity *= 2;
+        }
+        int i = percolateUp(size, work);
+        data[i] = work;
+        size++;
+    }
+
+    private int percolateUp(int index, E work) {
+        while (index > 0 && work.compareTo(data[parent(index)]) < 0) {
+            data[index] = data[parent(index)];
+            index = parent(index);
+        }
+        return index;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (hasWork()) {
+            return data[0];
+        }
+        // if it doesn't have work
+        throw new NoSuchElementException();
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        E work = peek(); // throws exception if it doesn't have work
+        int index = percolateDown(0, data[size - 1]);
+        data[index] = data[size - 1];
+        data[size - 1] = null;
+        size--;
+        return work;
+    }
+
+    private int percolateDown(int index, E work) {
+        while (4 * index + 1 < size) {
+            int minChild = minChildIndex(index);
+            if (data[minChild].compareTo(work) < 0) {
+                data[index] = data[minChild];
+                index = minChild;
+            } else {
+                break;
+            }
+        }
+        return index;
+    }
+
+    private int minChildIndex(int parent) {
+        int min = 4 * parent + 1;
+        for (int i = 1; i < 4; i++) {
+            if (min + i >= size) {
+                break;
+            }
+            if (data[4 * parent + 1 + i].compareTo(data[min]) < 0) {
+                min = 4 * parent + 1 + i;
+            }
+        }
+        return min;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        capacity = 10;
+        data = (E[])new Comparable[capacity];
+        size = 0;
+    }
+
+    private int parent(int child) {
+        return (child - 1) / 4;
     }
 }
