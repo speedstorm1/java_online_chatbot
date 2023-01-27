ec259e036b7167900264faa48c14829e02766396
Ayush <speedstorm123@gmail.com>
2023-01-26 20:54:55 -0800

    tried adding iterator for checkpoint 1

--- src/main/java/datastructures/dictionaries/MoveToFrontList.java
+++ src/main/java/datastructures/dictionaries/MoveToFrontList.java
@@ -1,8 +1,13 @@
 package datastructures.dictionaries;
 
 import cse332.datastructures.containers.Item;
+import cse332.datastructures.trees.BinarySearchTree;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
+import cse332.interfaces.misc.SimpleIterator;
+import cse332.interfaces.worklists.WorkList;
+import datastructures.worklists.ArrayStack;
+import datastructures.worklists.ListFIFOQueue;
 
 import java.util.Iterator;
 
@@ -20,18 +25,96 @@ import java.util.Iterator;
  * dictionary/list's iterator.
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
+    private ListNode root;
     @Override
     public V insert(K key, V value) {
-        throw new NotYetImplementedException();
+        if (value == null) {
+            throw new IllegalArgumentException();
+        }
+        if (findPrev(key) == null) { // list doesn't contain this key, so we just insert new node @ front
+            root = new ListNode(key, value, root.next);
+        }
+        V oldVal = find(key);
+        root.data = value;
+        size++;
+        return oldVal;
+    }
+
+
+    public ListNode findPrev(K key) { // returns the previous node of the node with the value
+        ListNode curr = root.next;
+        ListNode prev = root;
+        while (curr != null) {
+            if (curr.key.equals(key)) {
+                return prev;
+            }
+            prev = prev.next;
+            curr = curr.next;
+        }
+        return null;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        if (root == null) {
+            return null;
+        }
+        if (root.key.equals(key)) {
+            return (V) root.data;
+        }
+        ListNode prev = findPrev(key);
+        ListNode newRoot = prev.next;
+        if (prev == null) {
+            return null;
+        }
+        prev.next = prev.next.next;
+        newRoot.next = root;
+        root = newRoot;
+        return (V) root.data;
     }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
-        throw new NotYetImplementedException();
+        return new FLIterator();
+    }
+
+
+
+
+    private class FLIterator extends SimpleIterator<Item<K, V>> {
+        private MoveToFrontList.ListNode current;
+      //  private final WorkList<BinarySearchTree.BSTNode> nodes;
+
+        public FLIterator() {
+            if (root != null) {
+                this.current = root;
+            }
+//            this.nodes = new ArrayStack<BinarySearchTree.BSTNode>();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.current != null;
+        }
+
+        @Override
+        public Item<K, V> next() {
+            current = current.next;
+            return new Item(current.key, current.data);
+        }
+    }
+
+    private class ListNode<K, V> {
+        public K key;
+        public V data;
+        public MoveToFrontList.ListNode next;
+        public ListNode(K key, V data, MoveToFrontList.ListNode next) {
+            this.data = data;
+            this.next = next;
+            this.key = key;
+        }
     }
 }
