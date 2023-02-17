ac955f706ac5db9a71c0b0743fca8377726df696
Ayush <speedstorm123@gmail.com>
2023-02-17 11:06:27 -0800

    Working top k sort algorithm

--- src/main/java/p2/sorts/TopKSort.java
+++ src/main/java/p2/sorts/TopKSort.java
@@ -1,6 +1,7 @@
 package p2.sorts;
 
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 import java.util.Comparator;
 
@@ -13,6 +14,20 @@ public class TopKSort {
      * Behaviour is undefined when k > array.length
      */
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap sortHeap = new MinFourHeap(comparator);
+        System.out.println(sortHeap.size());
+        System.out.println(k);
+        for (E value : array) {
+            sortHeap.add(value);
+            if (sortHeap.size() > k) {
+                sortHeap.next();
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            array[i] = (E) sortHeap.next();
+        }
+        for (int i = k; i < array.length; i++) {
+            array[i] = null;
+        }
     }
 }
