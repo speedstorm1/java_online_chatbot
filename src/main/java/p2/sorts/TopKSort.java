6390c16c07bc0c3834a50fbd17d3b95dd3d972ff
Ayush <speedstorm123@gmail.com>
2023-02-17 13:46:29 -0800

    added check to top k sort

--- src/main/java/p2/sorts/TopKSort.java
+++ src/main/java/p2/sorts/TopKSort.java
@@ -14,20 +14,22 @@ public class TopKSort {
      * Behaviour is undefined when k > array.length
      */
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
-        MinFourHeap sortHeap = new MinFourHeap(comparator);
-        System.out.println(sortHeap.size());
-        System.out.println(k);
-        for (E value : array) {
-            sortHeap.add(value);
-            if (sortHeap.size() > k) {
-                sortHeap.next();
+        if (k <= array.length) {
+            MinFourHeap sortHeap = new MinFourHeap(comparator);
+            System.out.println(sortHeap.size());
+            System.out.println(k);
+            for (E value : array) {
+                sortHeap.add(value);
+                if (sortHeap.size() > k) {
+                    sortHeap.next();
+                }
             }
-        }
-        for (int i = 0; i < k; i++) {
-            array[i] = (E) sortHeap.next();
-        }
-        for (int i = k; i < array.length; i++) {
-            array[i] = null;
+            for (int i = 0; i < k; i++) {
+                array[i] = (E) sortHeap.next();
+            }
+            for (int i = k; i < array.length; i++) {
+                array[i] = null;
+            }
         }
     }
 }
