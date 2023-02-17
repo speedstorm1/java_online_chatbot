f76ba698bdbac847156835b227d3688d9d26ef90
Ayush <speedstorm123@gmail.com>
2023-02-16 22:31:55 -0800

    Correctly Attempted Heap Sort

--- src/main/java/p2/sorts/HeapSort.java
+++ src/main/java/p2/sorts/HeapSort.java
@@ -1,8 +1,10 @@
 package p2.sorts;
 
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class HeapSort {
     public static <E extends Comparable<E>> void sort(E[] array) {
@@ -10,6 +12,16 @@ public class HeapSort {
     }
 
     public static <E> void sort(E[] array, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap sortHeap = new MinFourHeap(comparator);
+        for (E value : array) {
+            sortHeap.add(value);
+        }
+
+        int counter = 0;
+        for (Object value : sortHeap) {
+            E castedValue = (E) value;
+            array[counter] = castedValue;
+            counter++;
+        }
     }
 }
